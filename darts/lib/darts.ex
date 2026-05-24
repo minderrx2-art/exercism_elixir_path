77ad6c0bab39fa8102e6a23334bd29c0dc6fbd48
Minvydas <minderrx2@gmail.com>
2026-05-24 13:50:31 +0100

    Exercises done

--- darts/lib/darts.ex
+++ darts/lib/darts.ex
@@ -6,5 +6,13 @@ defmodule Darts do
   """
   @spec score(position) :: integer
   def score({x, y}) do
+    distance = :math.sqrt(x * x + y * y)
+
+    cond do
+      distance <= 1 -> 10
+      distance <= 5 -> 5
+      distance <= 10 -> 1
+      true -> 0
+    end
   end
 end
