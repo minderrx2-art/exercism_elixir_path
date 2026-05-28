211fb37bc164c91b6a78f19d45c72c35282354b1
Minvydas <minderrx2@gmail.com>
2026-05-28 21:51:39 +0100

    Exercise done

--- space-age/lib/space_age.ex
+++ space-age/lib/space_age.ex
@@ -1,19 +1,28 @@
 defmodule SpaceAge do
-  @type planet ::
-          :mercury
-          | :venus
-          | :earth
-          | :mars
-          | :jupiter
-          | :saturn
-          | :uranus
-          | :neptune
+  @earth_year 31557600
+  @planets %{
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  }
 
   @doc """
   Return the number of years a person that has lived for 'seconds' seconds is
   aged on 'planet', or an error if 'planet' is not a planet.
   """
-  @spec age_on(planet, pos_integer) :: {:ok, float} | {:error, String.t()}
+
   def age_on(planet, seconds) do
+    case Map.get(@planets, planet) do
+      nil -> {:error, "not a planet"}
+      ratio ->
+        earth_age = seconds / @earth_year
+        {:ok, earth_age / ratio |> Float.round(2)}
+    end
   end
+
 end
